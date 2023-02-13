3ab83564c06a5885f9240515f4fee7b09e219e7f
cecil <cecilghimire@gmail.com>
2023-02-13 18:02:36 +0545

    removed util app

--- backend/util/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import BufferPolygonView
-from django.contrib import admin
-from django.urls import path,include
-urlpatterns = [
-    # ... other URLs ...
-    path('buffer_polygon/', BufferPolygonView.as_view(), name='buffer_polygon'),
-]
